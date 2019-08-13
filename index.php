f33540037bfd631fbb7705d9e678469cfd35228a
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2019-08-14 01:27:38 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    // set this variable acording to directory location
+    $config_link="";
+    include $config_link."includes/links_configuration.php";
+
+        include"includes/header.php"; // Database connection included in header
+        include"includes/slider.php";
+        include"includes/about_campus.php";
+        include"includes/news_and_circulars.php";
+        include"includes/departments.php";
+        //include"includes/campus_officers.php";
+        //include"includes/alumni_testimonials.php";
+        //include"includes/campus_qualities.php";
+        include"includes/footer.php";
+
+?>
+<!--Maintance Message for Public -->
+<!--
+<script> 
+$(function() {  $("#popModal").modal('show'); });
+</script>
+-->
+
+<!--
+Color Scheme
+#6a41ed
+#335BFF
+-->
+
+<!--
+Implement dynatable for every table in every section.
+-->
+<!--
+http://localhost/muetkhp.edu.pk%20(Development)/
+Live Preview link for Brackets
+/-->
